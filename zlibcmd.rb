cc04727f78b6368c9749f7ac9f99f192949a6db9
ebc-2in2crc <y015h1n80@gmail.com>
2019-05-18 10:19:52 +0900

    Change: v1.1

--- zlibcmd.rb
+++ zlibcmd.rb
@@ -1,9 +1,9 @@
 class Zlibcmd < Formula
   desc "Row zlib compression commandline program"
-  version "1.0"
+  version "1.1"
   homepage "https://github.com/ebc-2in2crc/zlibcmd"
   url "https://github.com/ebc-2in2crc/zlibcmd/releases/download/v#{version}/zlibcmd-#{version}.zip"
-  sha256 "be0f5908a2820867f048244dd8a7d0d410715a55e1757b8ef82d3a0fa200dd88"
+  sha256 "885053f352600dd0726f1694883415706e07b6613741a0b48b02d87743b6181f"
 
   def install
     bin.install "zlib"
