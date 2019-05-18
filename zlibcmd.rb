47d8726b5be428012075046f3b881e0225cc1eb9
ebc-2in2crc <y015h1n80@gmail.com>
2019-05-18 10:10:57 +0900

    refactor

--- zlibcmd.rb
+++ zlibcmd.rb
@@ -1,14 +1,15 @@
-ZLIBCMD_VERSION="1.0"
-ARCHIVE_BASENAME="zlibcmd-#{ZLIBCMD_VERSION}"
 class Zlibcmd < Formula
   desc "Row zlib compression commandline program"
+  version "1.0"
   homepage "https://github.com/ebc-2in2crc/zlibcmd"
-  url "https://github.com/ebc-2in2crc/zlibcmd/releases/download/v1.0/zlibcmd-1.0.zip"
+  url "https://github.com/ebc-2in2crc/zlibcmd/releases/download/v#{version}/zlibcmd-#{version}.zip"
   sha256 "be0f5908a2820867f048244dd8a7d0d410715a55e1757b8ef82d3a0fa200dd88"
 
   def install
     bin.install "zlib"
-	system "cp", "#{buildpath}/#{ARCHIVE_BASENAME}.jar", "#{bin}/#{ARCHIVE_BASENAME}.jar"
+
+    archiveBaseName="zlibcmd-#{version}"
+    system "cp", "#{buildpath}/#{archiveBaseName}.jar", "#{bin}/#{archiveBaseName}.jar"
   end
 
   test do
